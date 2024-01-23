f815231fcf4af283e7c9faf48d5e836f5a1ad8e2
Amazon_Project <r.nandhanraghu123@gmail.com>
2024-01-23 22:53:04 +0530

    radio buttton fixing

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -26,7 +26,7 @@ cart.forEach((item)=>{
                   ${matchedProduct.name}
                 </div>
                 <div class="product-price">
-                  ${matchedProduct.priceCents/100}
+                  $${(matchedProduct.priceCents/100).toFixed(2)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -48,7 +48,7 @@ cart.forEach((item)=>{
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchedProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -61,7 +61,7 @@ cart.forEach((item)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchedProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -74,7 +74,7 @@ cart.forEach((item)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchedProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
