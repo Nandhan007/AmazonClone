fb3f9cab167fa5d2b53cb65424c1840659eaa5b4
Amazon_Project <r.nandhanraghu123@gmail.com>
2024-01-24 22:32:22 +0530

    local storage added

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,10 +1,19 @@
-export let cart=[  {
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+
+if(!cart){
+    cart=[  {
         prodId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity: 5
     },{
          prodId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
          quantity: 10
      }];
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function add_Cart(ProductId){
     let matchedProduct;
@@ -26,6 +35,7 @@ export function add_Cart(ProductId){
          })
   }
   
+  saveToStorage();
   }
 
 export function removeProduct(ProductId){
@@ -37,4 +47,6 @@ export function removeProduct(ProductId){
     })
     cart = newcart;
     console.log(cart)
+
+    saveToStorage();
   }
